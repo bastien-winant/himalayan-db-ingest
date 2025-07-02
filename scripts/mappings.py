b52944f582a2e9882ba91fc06ab96158b8f4517a
bastien-winant <armand.winant@gmail.com>
2025-07-02 17:18:48 +0200

    store static mappings

---
+++ scripts/mappings.py
@@ -0,0 +1,45 @@
+himal_map = {
+	0: 'Unclassified',
+	1: 'Annapurna',
+	2: 'Api/Byas Risi/Guras',
+	3: 'Damodar',
+	4: 'Dhaulagiri',
+	5: 'Ganesh/Shringi',
+	6: 'Janak/Ohmi Kangri',
+	7: 'Jongsang',
+	8: 'Jugal',
+	9: 'Kangchenjunga/Simhalila',
+	10: 'Kanjiroba',
+    11: 'Kanti/Palchung',
+	12: 'Khumbu',
+	13: 'Langtang',
+	14: 'Makalu',
+	15: 'Manaslu/Mansiri',
+	16: 'Mukut/Mustang',
+	17: 'Nalakankar/Chandi/Changla',
+	18: 'Peri',
+	19: 'Rolwaling',
+	20: 'Saipal'
+}
+
+region_map = {
+	0: 'Unclassified',
+	1: 'Kangchenjunga-Janak',
+	2: 'Khumbu-Rolwaling-Makalu',
+	3: 'Langtang-Jugal',
+	4: 'Manaslu-Ganesh',
+	5: 'Annapurna-Damodar-Peri',
+	6: 'Dhaulagiri-Mukut',
+	7: 'Kanjiroba-Far West'
+}
+
+peak_host_map = {
+	0: 'Unclassified',
+	1: 'Nepal',
+	2: 'China',
+	3: 'India',
+	4: 'Nepal;China',
+	5: 'Nepal;India',
+	6: 'Nepal;China;India'
+}
+
