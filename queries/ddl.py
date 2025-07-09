ac122361ab1125a1d016810e0578a7b9df51ed2c
bastien-winant <armand.winant@gmail.com>
2025-07-09 13:51:38 +0200

    add DDL queries

--- queries/ddl.py
+++ queries/ddl.py
@@ -1,46 +1,64 @@
 countries_ddl_query = """
 	CREATE TABLE countries (
-		id SMALLINT PRIMARY KEY,
-		name TEXT
-	);
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100)
+);
 """
 
 regions_ddl_query = """
 	CREATE TABLE regions (
-		id SMALLINT PRIMARY KEY,
-		name TEXT
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(100)
+	);
+"""
+
+region_host_links_ddl_query = """
+	CREATE TABLE region_hosts (
+		region_id INTEGER,
+		host_id INTEGER,
+		FOREIGN KEY (region_id) REFERENCES regions (id),
+		FOREIGN KEY (host_id) REFERENCES countries (id)
 	);
 """
 
 mountains_ddl_query = """
 	CREATE TABLE mountains (
-		id SMALLINT PRIMARY KEY,
-		name TEXT
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(100)
+	);
+"""
+
+mountain_host_links_ddl_query = """
+	CREATE TABLE mountain_hosts (
+		mountain_id INTEGER,
+		host_id INTEGER,
+		FOREIGN KEY (mountain_id) REFERENCES mountains (id),
+		FOREIGN KEY (host_id) REFERENCES countries (id)
 	);
 """
 
 locations_ddl_query = """
 	CREATE TABLE locations (
-		id SMALLINT PRIMARY KEY,
+		id INTEGER PRIMARY KEY,
     name TEXT,
-    mountain_id SMALLINT,
+    mountain_id INTEGER,
     FOREIGN KEY (mountain_id) REFERENCES mountains (id)
 	);
 """
 
 peaks_ddl_query = """
 	CREATE TABLE peaks (
-		id CHAR(6),
+		id CHAR(6) PRIMARY KEY,
 		name TEXT,
-		location_id SMALLINT,
+		location_id INTEGER,
 		FOREIGN KEY (location_id) REFERENCES locations (id)
 	);
 """
 
 local_names_ddl_query = """
 	CREATE TABLE local_names (
-		peak_id,
-		name,
+		peak_id INTEGER,
+		name TEXT,
 		FOREIGN KEY (peak_id) REFERENCES peaks (id)
 	);
 """
