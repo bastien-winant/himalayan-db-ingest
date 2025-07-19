b8fc989d1c78d1af4dabecb0f005d1df5f5f8283
bastien-winant <armand.winant@gmail.com>
2025-07-19 23:49:09 +0200

    add internet gateway and route table

--- terraform/data-fetcher/vpc.tf
+++ terraform/data-fetcher/vpc.tf
@@ -6,9 +6,28 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "192.168.1.0/24"
+  map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "192.168.2.0/24"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "vpc_public_routing" {
+  route_table_id = aws_route_table.route_table.id
+  subnet_id = aws_subnet.public_subnet.id
 }
