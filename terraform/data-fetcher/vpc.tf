59b25f6019c76ac14e33e785c860c3cce26e74c3
bastien-winant <armand.winant@gmail.com>
2025-07-20 01:19:42 +0200

    define db security group

--- terraform/data-fetcher/vpc.tf
+++ terraform/data-fetcher/vpc.tf
@@ -1,3 +1,4 @@
+# virtual private network
 resource "aws_vpc" "vpc" {
   cidr_block = "192.168.0.0/16"
   instance_tenancy = "default"
@@ -9,10 +10,12 @@ resource "aws_subnet" "public_subnet" {
   map_public_ip_on_launch = true
 }
 
+# private subnet that will host the database
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.vpc.id
   cidr_block = "192.168.2.0/24"
 }
+
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
@@ -30,4 +33,27 @@ resource "aws_route_table" "route_table" {
 resource "aws_route_table_association" "vpc_public_routing" {
   route_table_id = aws_route_table.route_table.id
   subnet_id = aws_subnet.public_subnet.id
+}
+
+# control traffic to and from the database
+resource "aws_security_group" "db_sg" {
+  name = "allow_"
+  description = "Allow inbound traffic and outbound traffic"
+  vpc_id = aws_vpc.vpc.id
+}
+
+# Inbound rule: allow SSH inbound traffic
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_inbound" {
+  security_group_id = aws_security_group.db_sg.id
+  ip_protocol = "ssh"
+  from_port = 22
+  to_port = 22
+}
+
+# Allow all outbound traffic
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.db_sg.id
+  from_port = 0
+  to_port = 0
+  ip_protocol = "-1"
 }
