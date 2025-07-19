51d8682cdb7f4893709beb4fcd3eecde5c09c110
bastien-winant <armand.winant@gmail.com>
2025-07-19 23:06:54 +0200

    add private & public subnets

--- terraform/data-fetcher/vpc.tf
+++ terraform/data-fetcher/vpc.tf
@@ -2,3 +2,13 @@ resource "aws_vpc" "vpc" {
   cidr_block = "192.168.0.0/16"
   instance_tenancy = "default"
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "192.168.1.0/24"
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "192.168.2.0/24"
+}
